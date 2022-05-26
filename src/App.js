fc15efcec702ec02110d8627b400cbf24ae7c22c
David Abol Ger <draker74@gmail.com>
2022-05-26 19:29:58 +0300

    Hooks in React

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import './App.css';
 // eslint-disable-next-line import/no-named-as-default,import/no-named-as-default-member
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,62 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0, next: null, operation: null,
-    };
-    this.clickEvent = this.clickEvent.bind(this);
-  }
+function Calculator() {
+  const [state, setState] = useState({
+    obj: {
+      total: 0,
+      next: null,
+      operation: null,
 
-  clickEvent(e) {
-    this.setState((state) => ({
-      ...state,
-      ...calculate(state, e.target.name),
-    }));
-  }
+    },
+  });
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
+  const clickEvent = (name) => setState(({ obj }) => ({ obj: calculate(obj, name) }));
 
-      <div className="app1">
-        <div className="calculator">
-          <div className="display">
-            { total}
-            {operation}
-            {next}
+  return (
 
-          </div>
-          <div className="operators">
-            <button onClick={this.clickEvent} name="÷" type="button" color="primary" value="">&#247;</button>
-            <button onClick={this.clickEvent} name="x" type="button" color="primary" value="">&times;</button>
-            <button onClick={this.clickEvent} name="-" type="button" color="primary" value="">-</button>
-            <button onClick={this.clickEvent} name="+" type="button" color="primary" value="">+</button>
-            <button onClick={this.clickEvent} name="=" type="button" color="primary" value="">=</button>
-          </div>
+    <div className="app1">
+      <div className="calculator">
+        <div className="display">
+          { state.obj.total }
+          { state.obj.operation }
+          { state.obj.next }
 
-          <div className="numbers">
-            <button onClick={this.clickEvent} type="button" color="primary" name="AC">AC</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="">+/-</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="">%</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="2">2</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="3">3</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="4">4</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="5">5</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="6">6</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="7">7</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="8">8</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="9">9</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name="0">0</button>
-            <button onClick={this.clickEvent} type="button" color="primary" name=".">.</button>
-          </div>
+        </div>
+        <div className="operators">
+          <button onClick={clickEvent} name="÷" type="button" color="primary" value="">&#247;</button>
+          <button onClick={clickEvent} name="x" type="button" color="primary" value="">&times;</button>
+          <button onClick={clickEvent} name="-" type="button" color="primary" value="">-</button>
+          <button onClick={clickEvent} name="+" type="button" color="primary" value="">+</button>
+          <button onClick={clickEvent} name="=" type="button" color="primary" value="">=</button>
+        </div>
+
+        <div className="numbers">
+          <button onClick={clickEvent} type="button" color="primary" name="AC">AC</button>
+          <button onClick={clickEvent} type="button" color="primary" name="">+/-</button>
+          <button onClick={clickEvent} type="button" color="primary" name="%">%</button>
+          <button onClick={clickEvent} type="button" color="primary" name="1">1</button>
+          <button onClick={clickEvent} type="button" color="primary" name="2">2</button>
+          <button onClick={clickEvent} type="button" color="primary" name="3">3</button>
+          <button onClick={clickEvent} type="button" color="primary" name="4">4</button>
+          <button onClick={clickEvent} type="button" color="primary" name="5">5</button>
+          <button onClick={clickEvent} type="button" color="primary" name="6">6</button>
+          <button onClick={clickEvent} type="button" color="primary" name="7">7</button>
+          <button onClick={clickEvent} type="button" color="primary" name="8">8</button>
+          <button onClick={clickEvent} type="button" color="primary" name="9">9</button>
+          <button onClick={clickEvent} type="button" color="primary" name="0">0</button>
+          <button onClick={clickEvent} type="button" color="primary" name=".">.</button>
         </div>
       </div>
+    </div>
 
-    );
-  }
+  );
 }
 export default Calculator;
